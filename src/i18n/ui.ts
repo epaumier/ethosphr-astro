49638ffdc996d0faf18548f79fceae0b180fe372
tedd <paumier.edouard@gmail.com>
2023-04-27 19:21:20 +0200

    This first commit begins the implementation of internationalization, by using the corresponding documentation on Astro (https://docs.astro.build/en/recipes/i18n/).
    It also adds a light touch of customization on this template. There will be
    many more to come.

---
+++ src/i18n/ui.ts
@@ -0,0 +1,26 @@
+export const languages = {
+    en: 'English',
+    fr: 'Français',
+  };
+
+  export const defaultLang = 'en';
+
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.expertise': 'Expertise/Missions',
+      'nav.contribute': 'Contribute',
+      'nav.team': 'Our team',
+      'nav.blog': 'Blog',
+      'nav.contact': 'Contact',
+    },
+    fr: {
+      'nav.home': 'Accueil',
+      'nav.expertise': 'Expertises/Missions',
+      'nav.contribute': 'Participer',
+      'nav.team': 'Notre équipe',
+      'nav.blog': 'Blog',
+      'nav.contact': 'Contact',
+
+    },
+  } as const;
