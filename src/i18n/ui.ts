5c4a438370bc0568d751db55c17d83bf346d2437
tedd <paumier.edouard@gmail.com>
2023-05-22 23:08:35 +0200

    commit will be amended

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,16 +8,20 @@ export const languages = {
   export const ui = {
     en: {
       'nav.home': 'Home',
-      'nav.expertise': 'Expertise/Missions',
+      'nav.expertise': 'Expertise',
+      'nav.missions': 'Missions',
       'nav.contribute': 'Contribute',
+      'nav.contribute.adherer': 'Membership',
       'nav.team': 'Our team',
       'nav.blog': 'Blog',
       'nav.contact': 'Contact',
     },
     fr: {
       'nav.home': 'Accueil',
-      'nav.expertise': 'Expertises/Missions',
+      'nav.expertise': 'Expertises',
+      'nav.missions': 'Missions',
       'nav.contribute': 'Participer',
+      'nav.contribute.adherer': 'Adhérer',
       'nav.team': 'Notre équipe',
       'nav.blog': 'Blog',
       'nav.contact': 'Contact',
