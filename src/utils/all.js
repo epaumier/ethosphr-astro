bc541dec8aa5ed46ac2e99236347a12bb28ddacd
edouardp <contact@paumier-edouard.com>
2023-10-24 11:38:03 +0200

    blog articles from sanity 	- currently using a library for block content but not super happy: in sanity, videos are files and are stored outside of the block. this means I have to display videos outside of the flow of the article 	- posts do not have a language path as they will not be translated ( i think )

--- src/utils/all.js
+++ src/utils/all.js
@@ -8,10 +8,11 @@ export const getFormattedDate = (date) =>
       })
     : "";
 
-const sanityURL = import.meta.env.SANITY_URL;
+const sanityUrlImg = import.meta.env.SANITY_URL_IMG;
+const sanityUrlFiles = import.meta.env.SANITY_URL_FILES;
 
 export const getSanityImageURL = (ref) => {
-  // example of a file ref
+  // example of an image ref
   // image-a0dd108b7c2dffae7dbca28da131aa3bb34be09b-381x381-png
 
   const lastDashIndex = ref.lastIndexOf('-');
@@ -19,5 +20,17 @@ export const getSanityImageURL = (ref) => {
   // replacing the last dash with a dot to get the file format
   const fileFormat = ref.slice(lastDashIndex).replace('-', '.');
 
-  return sanityURL + ref.slice(6, -4) + fileFormat;
+  return sanityUrlImg + ref.slice(6, -4) + fileFormat;
+};
+
+export const getSanityFileURL = (ref) => {
+  // example of a file ref
+  // file-19eafda15ec9d11dad2752a2bb5d72b8c260a85b-m4v
+
+  const lastDashIndex = ref.lastIndexOf('-');
+
+  // replacing the last dash with a dot to get the file format
+  const fileFormat = ref.slice(lastDashIndex).replace('-', '.');
+
+  return sanityUrlFiles + ref.slice(5, -4) + fileFormat;
 };
