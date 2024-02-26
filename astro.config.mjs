1cf816e0b4aab4119369d9af1d8e79d6f7c68f4b
Nemikolh <Nemikolh@users.noreply.github.com>
2024-02-26 21:00:23 +0000

    fix: remove alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,9 +8,8 @@ export default defineConfig({
   integrations: [
     tailwind(),
     mdx(),
-    alpinejs()
   ],
   redirects: {
     '/': '/fr/home/'
-  }
+  },
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-declare global {
-    var myString: string;
-    function myFunction(): URL;
-}
-
-global.myFunction = () => {
-    return window.location.href;
-}
-
-    export {};
